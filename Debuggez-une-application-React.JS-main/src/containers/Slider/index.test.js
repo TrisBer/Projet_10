2c406313605fbaf88d869a328b2b088fdb808e82
Naru <tristan.etude@gmail.com>
2024-05-09 18:53:13 +0200

    Correction EventCard/index

--- Debuggez-une-application-React.JS-main/src/containers/Slider/index.test.js
+++ Debuggez-une-application-React.JS-main/src/containers/Slider/index.test.js
@@ -5,25 +5,25 @@ import { api, DataProvider } from "../../contexts/DataContext";
 const data = {
   focus: [
     {
-      title: "World economic forum",
-      description:
-        "Oeuvre à la coopération entre le secteur public et le privé.",
-      date: "2022-02-29T20:28:45.744Z",
-      cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
+        title: "World economic forum",
+        description:
+            "Oeuvre à la coopération entre le secteur public et le privé.",
+        date: "2022-02-29T20:28:45.744Z",
+        cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
     },
     {
-      title: "World Gaming Day",
-      description: "Evenement mondial autour du gaming",
-      date: "2022-03-29T20:28:45.744Z",
-      cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
+        title: "World Gaming Day",
+        description: "Evenement mondial autour du gaming",
+        date: "2022-03-29T20:28:45.744Z",
+        cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
     },
     {
-      title: "World Farming Day",
-      description: "Evenement mondial autour de la ferme",
-      date: "2022-01-29T20:28:45.744Z",
-      cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
+        title: "World Farming Day",
+        description: "Evenement mondial autour de la ferme",
+        date: "2022-01-29T20:28:45.744Z",
+        cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
     },
-  ],
+],
 };
 
 describe("When slider is created", () => {
@@ -31,14 +31,14 @@ describe("When slider is created", () => {
     window.console.error = jest.fn();
     api.loadData = jest.fn().mockReturnValue(data);
     render(
-      <DataProvider>
-        <Slider />
-      </DataProvider>
+        <DataProvider>
+            <Slider />
+        </DataProvider>
     );
-    await screen.findByText("World economic forum");
-    await screen.findByText("janvier");
-    await screen.findByText(
-      "Oeuvre à la coopération entre le secteur public et le privé."
+    await screen.findAllByText("World economic forum");
+    await screen.findAllByText("février");
+    await screen.findAllByText(
+        "Oeuvre à la coopération entre le secteur public et le privé."
     );
   });
 });
