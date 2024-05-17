5b92ea6d2e1f165c3bb7dafb44db22ce8baa45e7
Naru <tristan.etude@gmail.com>
2024-05-17 16:51:07 +0200

    Correction formulaire de contact

--- Debuggez-une-application-React.JS-main/src/containers/Form/index.test.js
+++ Debuggez-une-application-React.JS-main/src/containers/Form/index.test.js
@@ -8,18 +8,18 @@ describe("When Events is created", () => {
     await screen.findByText("Nom");
     await screen.findByText("Prénom");
     await screen.findByText("Personel / Entreprise");
-  });
+});
 
-  describe("and a click is triggered on the submit button", () => {
-    it("the success action is called", async () => {
+describe("and a click is triggered on the submit button", () => {
+  it("the success action is called", async () => {
       const onSuccess = jest.fn();
       render(<Form onSuccess={onSuccess} />);
       fireEvent(
-        await screen.findByTestId("button-test-id"),
-        new MouseEvent("click", {
-          cancelable: true,
-          bubbles: true,
-        })
+          await screen.findByTestId("button-test-id"),
+          new MouseEvent("click", {
+              cancelable: true,
+              bubbles: true,
+          })
       );
       await screen.findByText("En cours");
       await screen.findByText("Envoyer");
